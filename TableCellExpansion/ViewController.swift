0da1dcbeabc1d20f0a3bcc1bb86887bb8927579b
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2019-11-01 19:51:23 +0200

    demo

--- TableCellExpansion/ViewController.swift
+++ TableCellExpansion/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private lazy var data: [Model] = (0..<100).map {
+        return Model(text: "Row \($0)", isExpanded: false)
     }
-
-
+    
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! Cell
+        cell.dataSource = { [unowned self] in
+            return self.data[indexPath.row]
+        }
+        return cell
+    }
+    
+    // MARK: UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        data[indexPath.row].isExpanded = !data[indexPath.row].isExpanded
+        
+        (tableView.cellForRow(at: indexPath) as! Cell).refresh()
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
 }
 
+class Cell: UITableViewCell {
+    
+    func refresh() {
+        model = dataSource?()
+    }
+    var dataSource: (() -> Model)? {
+        didSet {
+            refresh()
+        }
+    }
+    private var model: Model? {
+        didSet {
+            label.text = model?.text
+            dropDown.isHidden = !(model?.isExpanded ?? false)
+        }
+    }
+    
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var dropDown: UIStackView!
+}
+
+
+struct Model {
+    let text: String
+    var isExpanded: Bool
+}
